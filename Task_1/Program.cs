2462804be0ee2e10e1f8a52f0b9e324d2e86cf71
Sergey <runix1981@gmail.com>
2023-03-04 12:14:47 +0300

    Add lesson tasks for independent work

---
+++ Task_1/Program.cs
@@ -0,0 +1,65 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// Функция создания массива
+
+int[] AddArray(string msg)
+{
+    System.Console.Write($"{msg} -> ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    return array;
+}
+
+// Функция заполнения массива числами с клавиатуры
+
+int[] EnterArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"Введите {i + 1} элемент массива -> ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+// Функция вывода массива в консоль
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+}
+
+// Функция подсчета чисел больше 0 
+
+int MoreZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Функция вывода результата в консоль
+
+void PrintRes(int number)
+{
+    System.Console.Write($"-> {number}");
+}
+
+int[] array = AddArray("Введите размер массива");
+EnterArray(array);
+PrintArray(array);
+int res = MoreZero(array);
+PrintRes(res);
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Функция ввода данных пользователем
+
+double Prompt(string msg)
+{
+    System.Console.Write($"{msg} -> ");
+    double value;
+    if (double.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.Write("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+double b1 = Prompt("Введите значение b1");
+double k1 = Prompt("Введите значение k1");
+double b2 = Prompt("Введите значение b2");
+double k2 = Prompt("Введите значение k2");
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+double FindY(double b1, double k1, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+System.Console.WriteLine($"Координаты пересечения двух прямых равны (X  {FindX(b1,k1,b2,k2)} | Y  {FindY(b1,k1,FindX(b1,k1,b2,k2))})");
+
